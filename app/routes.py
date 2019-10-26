64d040fbaebae771c18245968b587b98ade9efa5
Your Name <you@berkeley.edu>
2019-10-25 21:44:32 -0700

    added html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    taskExample = {'Title': 'Watch webcast', 'Time': '45'}
+    taskList = [
+        {
+            'Title': 'Bio Reading',
+            'Time': '15'
+        },
+        {
+            'Title': 'Problem Set',
+            'Time': '20'
+        }
+    ]
+    return render_template('index.html', title='Home', task=taskExample, taskList=taskList)
