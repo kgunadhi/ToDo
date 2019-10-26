40d5e5826684bee7292fffea58e662aa8c9f3926
Your Name <you@berkeley.edu>
2019-10-26 16:19:50 -0700

    added finished button, can delete tasks from db

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
     taskTitle = StringField('Task Title', validators=[DataRequired()])
     taskTime = StringField('Time to complete task', validators=[DataRequired()])
     submit = SubmitField('Add')
+
+class FinishedForm(FlaskForm):
+    submit = SubmitField('Finished with top task')
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,23 @@
 from flask import render_template, flash, redirect
 from app import app, db
-from app.forms import TaskForm
+from app.forms import TaskForm, FinishedForm
 from app.models import Task
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = TaskForm()
-    if form.validate_on_submit():
-        task = Task(title=form.taskTitle.data, minutes=form.taskTime.data)
+    form1 = TaskForm()
+    if form1.validate_on_submit():
+        task = Task(title=form1.taskTitle.data, minutes=form1.taskTime.data)
         db.session.add(task)
         db.session.commit()
         return redirect('/index')
+    form2 = FinishedForm()
+    if form2.validate_on_submit():
+        first = Task.query.first()
+        if first:
+            db.session.delete(first)
+        db.session.commit()
+        return redirect('/index')
     taskList = Task.query.all()
-    return render_template('index.html', title='Home', taskList=taskList, form=form)
+    return render_template('index.html', title='Home', taskList=taskList, form1=form1, form2=form2)
