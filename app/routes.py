6637daaf7fa91d9722e5711f178ca403cb5146fc
Your Name <you@berkeley.edu>
2019-10-26 14:02:53 -0700

    db collects tasks entered

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,16 @@
 from flask import render_template, flash, redirect
-from app import app
+from app import app, db
 from app.forms import TaskForm
+from app.models import Task
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    taskExample = {'Title': 'Watch webcast', 'Time': '45'}
-    taskList = [
-        {
-            'Title': 'Bio Reading',
-            'Time': '15'
-        },
-        {
-            'Title': 'Problem Set',
-            'Time': '20'
-        }
-    ]
-
-    taskListSoft = []
     form = TaskForm()
     if form.validate_on_submit():
-        flash('Task to complete: {}, in {} minutes'.format(
-            form.taskTitle.data, form.taskTime.data))
-        taskListSoft.append({'Title': form.taskTitle.data, 'Time': form.taskTime.data})
+        task = Task(title=form.taskTitle.data, minutes=form.taskTime.data)
+        db.session.add(task)
+        db.session.commit()
         return redirect('/index')
-    return render_template('index.html', title='Home', task=taskExample, taskList=taskListSoft, form=form)
+    taskList = Task.query.all()
+    return render_template('index.html', title='Home', taskList=taskList, form=form)
