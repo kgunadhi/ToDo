758dca9811dbcfe38fde22e0d4193ea2f682034a
Kaitlyn <kgunadhi@gmail.com>
2019-10-27 02:00:50 -0700

    more styling

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,10 @@ from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class TaskForm(FlaskForm):
-    taskTitle = StringField('Task Title', validators=[DataRequired()])
-    taskTime = StringField('Time to complete task', validators=[DataRequired()])
+    taskTitle = StringField('Task name', validators=[DataRequired()])
+    taskTime = StringField('Minutes to complete task', validators=[DataRequired()])
     submit = SubmitField('Add')
 
 class FinishedForm(FlaskForm):
-    taskFinished = BooleanField('Finished the top task? ', validators=[DataRequired()])
+    taskFinished = BooleanField('Completed first task?', validators=[DataRequired()])
     submit = SubmitField('Yay!')
